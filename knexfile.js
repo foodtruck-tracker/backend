c0a50caacd2067dceb6d0b8c4464b1918c9ef0c6
Brad <Brad@BRADSPC>
2020-01-08 17:07:24 -0700

    changing knexfile path

--- knexfile.js
+++ knexfile.js
@@ -5,29 +5,13 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      filename: './data/dev.sqlite3'
     }
   },
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || {filename: "dev.sqlite3"},
+    connection: process.env.DATABASE_URL || {filename: "./data/dev.sqlite3"},
     pool: {
       min: 2,
       max: 10
